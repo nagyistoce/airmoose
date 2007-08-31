b1c5e6d87a4d0b4d3ee2a17c27d420ce10a39109
ggarside <ggarside@1931a908-4738-0410-b987-770cfd790793>
2007-08-31 16:12:15 +0000

    Add new preferences window with toolbar. Hook into MainController and ASCore. Some accidental reflows of text.

---
+++ trunk/PrefsWindow.h
@@ -0,0 +1,16 @@
+//
+//  PrefWindow.h
+//  AirMoose
+//
+//  Created by Geoffrey Garside on 31/08/2007.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PrefsWindow : NSWindow
+{
+}
+
+@end
