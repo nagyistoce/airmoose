c29bb777f8ebce2423855b8acbd622b98af67260
ggarside <ggarside@1931a908-4738-0410-b987-770cfd790793>
2007-09-01 09:17:07 +0000

    Reflow and update comments in some of my source files

--- trunk/PrefsController.m
+++ trunk/PrefsController.m
@@ -3,7 +3,7 @@
 //  AirMoose
 //
 //  Created by Geoffrey Garside on 31/08/2007.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//  Copyright 2007 AirMoose. All rights reserved.
 //
 
 #import "PrefsController.h"
@@ -30,9 +30,9 @@ static PrefsController * _sharedPrefsController = nil;
 
 + (PrefsController *) sharedPrefsController
 {
-	if (_sharedPrefsController == nil)
-		_sharedPrefsController = [[PrefsController alloc] initWithWindowNibName: @"PrefsWindow"];
-	return _sharedPrefsController;
+    if (_sharedPrefsController == nil)
+        _sharedPrefsController = [[PrefsController alloc] initWithWindowNibName: @"PrefsWindow"];
+    return _sharedPrefsController;
 }
 
 - (id) initWithWindowNibName:(NSString *) windowNibName
@@ -40,18 +40,18 @@ static PrefsController * _sharedPrefsController = nil;
     self = [super initWithWindowNibName: windowNibName];
     
     if (self != nil)
-	{
+    {
         _userDefaults = [NSUserDefaults standardUserDefaults];
-		
-		// Set the version
-		[currentVersionTextField setStringValue: @"1.1"];
+        
+        // Set the version
+        [currentVersionTextField setStringValue: @"1.1"];
         
         // Get the system sounds
         NSMutableArray *sounds = [NSMutableArray array];
         NSEnumerator *soundsEnumerator;
         
         if (soundsEnumerator = [[NSFileManager defaultManager] enumeratorAtPath: @"/System/Library/Sounds"])
-		{
+        {
             NSString *sound;
             while ((sound = [soundsEnumerator nextObject]))
             {
@@ -61,71 +61,71 @@ static PrefsController * _sharedPrefsController = nil;
             }
         }
         
-		systemSounds = [[sounds sortedArrayUsingSelector: @selector(caseInsensitiveCompare:)] retain];
+        systemSounds = [[sounds sortedArrayUsingSelector: @selector(caseInsensitiveCompare:)] retain];
     }
-	
-	return self;
+    
+    return self;
 }
 
 - (void) dealloc
 {
-	[_sparkleIntervalAllowedIdentifiers dealloc];
-	[systemSounds dealloc];
-	[super dealloc];
+    [_sparkleIntervalAllowedIdentifiers dealloc];
+    [systemSounds dealloc];
+    [super dealloc];
 }
 
 - (void) awakeFromNib
 {
-	NSToolbar *toolbar = [[NSToolbar alloc] initWithIdentifier: @"AirmoosePreferencesToolbar"];
-	[toolbar setDelegate: self];
-	[toolbar setAllowsUserCustomization: NO];
-	[toolbar setDisplayMode: NSToolbarDisplayModeIconAndLabel];
-	[toolbar setSizeMode: NSToolbarSizeModeRegular];
-	[[self window] setToolbar: toolbar];
-	[[self window] setShowsToolbarButton: NO];
-	
-	[toolbar setSelectedItemIdentifier: TOOLBAR_GENERAL];
-	[self setPrefView: nil];
+    NSToolbar *toolbar = [[NSToolbar alloc] initWithIdentifier: @"AirmoosePreferencesToolbar"];
+    [toolbar setDelegate: self];
+    [toolbar setAllowsUserCustomization: NO];
+    [toolbar setDisplayMode: NSToolbarDisplayModeIconAndLabel];
+    [toolbar setSizeMode: NSToolbarSizeModeRegular];
+    [[self window] setToolbar: toolbar];
+    [[self window] setShowsToolbarButton: NO];
+    
+    [toolbar setSelectedItemIdentifier: TOOLBAR_GENERAL];
+    [self setPrefView: nil];
 }
 
 - (NSToolbarItem *) toolbar: (NSToolbar *) toolbar
-	  itemForItemIdentifier: (NSString *) identifier
+      itemForItemIdentifier: (NSString *) identifier
   willBeInsertedIntoToolbar: (BOOL) flag
 {
-	NSToolbarItem *item;
-	item = [[NSToolbarItem alloc] initWithItemIdentifier: identifier];
-	
-	if ([identifier isEqualToString: TOOLBAR_GENERAL])
-	{
-		[item setLabel: NSLocalizedString(@"General", @"Preferences: General toolbar item title")];
-		[item setImage: [NSImage imageNamed: @"Pref.tif"]];
-		[item setTarget: self];
-		[item setAction: @selector(setPrefView:)];
-		[item setAutovalidates: NO];
-	}
-	else if ([identifier isEqualToString: TOOLBAR_SOUNDS])
-	{
-		[item setLabel: NSLocalizedString(@"Sounds", @"Preferences: Sounds toolbar item title")];
-		[item setImage: [NSImage imageNamed: @"SoundPref.tif"]];
-		[item setTarget: self];
-		[item setAction: @selector(setPrefView:)];
-		[item setAutovalidates: NO];
-	}
-	else if ([identifier isEqualToString: TOOLBAR_UPDATER])
-	{
-		[item setLabel: NSLocalizedString(@"Update", @"Preferences: Update toolbar item title")];
-		[item setImage: [NSImage imageNamed: @"UpdatePref.tif"]];
-		[item setTarget: self];
-		[item setAction: @selector(setPrefView:)];
-		[item setAutovalidates: NO];
-	}
-	else
-	{
-		[item release];
-		return nil;
-	}
-	
-	return item;
+    NSToolbarItem *item;
+    item = [[NSToolbarItem alloc] initWithItemIdentifier: identifier];
+    
+    if ([identifier isEqualToString: TOOLBAR_GENERAL])
+    {
+        [item setLabel: NSLocalizedString(@"General", @"Preferences: General toolbar item title")];
+        [item setImage: [NSImage imageNamed: @"Pref.tif"]];
+        [item setTarget: self];
+        [item setAction: @selector(setPrefView:)];
+        [item setAutovalidates: NO];
+    }
+    else if ([identifier isEqualToString: TOOLBAR_SOUNDS])
+    {
+        [item setLabel: NSLocalizedString(@"Sounds", @"Preferences: Sounds toolbar item title")];
+        [item setImage: [NSImage imageNamed: @"SoundPref.tif"]];
+        [item setTarget: self];
+        [item setAction: @selector(setPrefView:)];
+        [item setAutovalidates: NO];
+    }
+    else if ([identifier isEqualToString: TOOLBAR_UPDATER])
+    {
+        [item setLabel: NSLocalizedString(@"Update", @"Preferences: Update toolbar item title")];
+        [item setImage: [NSImage imageNamed: @"UpdatePref.tif"]];
+        [item setTarget: self];
+        [item setAction: @selector(setPrefView:)];
+        [item setAutovalidates: NO];
+    }
+    else
+    {
+        [item release];
+        return nil;
+    }
+    
+    return item;
 }
 
 - (NSArray *) toolbarSelectableItemIdentifiers: (NSToolbar *) toolbar
@@ -146,42 +146,42 @@ static PrefsController * _sharedPrefsController = nil;
 // TODO: Check to see if using these in KeyValueCoding might actually be causing a memory leak
 - (NSArray *) sparkleIntervalAllowedItemIdentifiers
 {
-//	if (_sparkleIntervalAllowedIdentifiers == nil)
-//		_sparkleIntervalAllowedIdentifiers = [NSArray arrayWithObjects: SPARKLE_DAILY, SPARKLE_WEEKLY, SPARKLE_NEVER, nil];
-//	return _sparkleIntervalAllowedIdentifiers;
-	return [NSArray arrayWithObjects: SPARKLE_DAILY, SPARKLE_WEEKLY, SPARKLE_NEVER, nil];
+    //	if (_sparkleIntervalAllowedIdentifiers == nil)
+    //		_sparkleIntervalAllowedIdentifiers = [NSArray arrayWithObjects: SPARKLE_DAILY, SPARKLE_WEEKLY, SPARKLE_NEVER, nil];
+    //	return _sparkleIntervalAllowedIdentifiers;
+    return [NSArray arrayWithObjects: SPARKLE_DAILY, SPARKLE_WEEKLY, SPARKLE_NEVER, nil];
 }
 
 - (int) sparkleIntervalFromIdentifier: (NSString *) identifier
 {
-	int interval = 0;
-	
-	if ([identifier isEqualToString: SPARKLE_DAILY])
-		interval = SPARKLE_DAILY_VALUE;
-	else if ([identifier isEqualToString: SPARKLE_WEEKLY])
-		interval = SPARKLE_WEEKLY_VALUE;
-	
-	return interval;
+    int interval = 0;
+    
+    if ([identifier isEqualToString: SPARKLE_DAILY])
+        interval = SPARKLE_DAILY_VALUE;
+    else if ([identifier isEqualToString: SPARKLE_WEEKLY])
+        interval = SPARKLE_WEEKLY_VALUE;
+    
+    return interval;
 }
 
 - (NSString *) sparkleInterval
 {
-	int interval = [[NSUserDefaults standardUserDefaults] integerForKey: SUScheduledCheckIntervalKey];
-	
-	if (interval == SPARKLE_DAILY_VALUE)
-		return SPARKLE_DAILY;
-	else if (interval == SPARKLE_WEEKLY_VALUE)
-		return SPARKLE_WEEKLY;
-	else
-		return SPARKLE_NEVER;
+    int interval = [[NSUserDefaults standardUserDefaults] integerForKey: SUScheduledCheckIntervalKey];
+    
+    if (interval == SPARKLE_DAILY_VALUE)
+        return SPARKLE_DAILY;
+    else if (interval == SPARKLE_WEEKLY_VALUE)
+        return SPARKLE_WEEKLY;
+    else
+        return SPARKLE_NEVER;
 }
 
 - (IBAction) setSparkleInterval: (id) sender
 {
-	// We can set the user default to 0 and it is the same as being off - we hope
-	[sparkleUpdater scheduleCheckWithInterval: [self sparkleIntervalFromIdentifier: sender]];
-	[[NSUserDefaults standardUserDefaults] setInteger: [self sparkleIntervalFromIdentifier: sender] 
-											   forKey: SUScheduledCheckIntervalKey];
+    // We can set the user default to 0 and it is the same as being off - we hope
+    [sparkleUpdater scheduleCheckWithInterval: [self sparkleIntervalFromIdentifier: sender]];
+    [[NSUserDefaults standardUserDefaults] setInteger: [self sparkleIntervalFromIdentifier: sender] 
+                                               forKey: SUScheduledCheckIntervalKey];
 }
 
 @end
@@ -190,8 +190,8 @@ static PrefsController * _sharedPrefsController = nil;
 
 - (void) setPrefView: (id) sender
 {
-	NSView *view = generalView;
-	
+    NSView *view = generalView;
+    
     if (sender)
     {
         NSString * identifier = [sender itemIdentifier];
@@ -207,15 +207,15 @@ static PrefsController * _sharedPrefsController = nil;
         return;
     
     NSRect windowRect = [window frame];
-	
-	float x_diff = ([view frame].size.width - [[window contentView] frame].size.width) * [window userSpaceScaleFactor];
+    
+    float x_diff = ([view frame].size.width - [[window contentView] frame].size.width) * [window userSpaceScaleFactor];
     windowRect.origin.x -= x_diff;
     windowRect.size.width += x_diff;
-	
+    
     float y_diff = ([view frame].size.height - [[window contentView] frame].size.height) * [window userSpaceScaleFactor];
     windowRect.origin.y -= y_diff;
     windowRect.size.height += y_diff;
-	
+    
     
     [view setHidden: YES];
     [window setContentView: view];
@@ -232,13 +232,13 @@ static PrefsController * _sharedPrefsController = nil;
         NSEnumerator * enumerator = [[toolbar items] objectEnumerator];
         NSToolbarItem * item;
         while ((item = [enumerator nextObject]))
-		{
+        {
             if ([[item itemIdentifier] isEqualToString: itemIdentifier])
             {
                 [window setTitle: [item label]];
                 break;
             }
-		}
+        }
     }
 }
 
